70f2970b400feed857a58078f09650a88db70d05
Igor Palmieri <idupalmieri@hotmail.com>
2025-04-08 17:24:08 -0300

    segundo commit

--- PrimeiroExercicioPython_Entrevista.py
+++ PrimeiroExercicioPython_Entrevista.py
@@ -3,9 +3,9 @@ nome= input("Qual o seu nome?")
 idade = input("Qual sua idade?")
 
 #Conversor idade
-idadeFuturo = int(idade) + 5
+idadeFuturo = int(idade) + 6
 
 #Exibe resultados do código
 print("Seu nome é : " + nome)
 print("Seu nome tem: " + str(len(nome)) + " letras !")
-print("Daqui a 5 anos voce tera:" + str(idadeFuturo) + "anos!" )
+print("Daqui a 6 anos voce tera:" + str(idadeFuturo) + "anos!" )
