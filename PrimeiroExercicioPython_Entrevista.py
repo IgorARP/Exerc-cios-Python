f7d91d944fe21ba241958d3710a19fdea598dd75
Igor Palmieri <idupalmieri@hotmail.com>
2025-04-08 17:12:23 -0300

    Primeiro commit

---
+++ PrimeiroExercicioPython_Entrevista.py
@@ -0,0 +1,11 @@
+#Pega input usuário
+nome= input("Qual o seu nome?")
+idade = input("Qual sua idade?")
+
+#Conversor idade
+idadeFuturo = int(idade) + 5
+
+#Exibe resultados do código
+print("Seu nome é : " + nome)
+print("Seu nome tem: " + str(len(nome)) + " letras !")
+print("Daqui a 5 anos voce tera:" + str(idadeFuturo) + "anos!" )
